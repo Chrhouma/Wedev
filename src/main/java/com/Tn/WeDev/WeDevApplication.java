36037a37d3d95640bad048c82d35c382a3ff73d9
Chrhouma <>
2019-07-21 18:29:49 +0200

    first commit
    Signed-off-by: Chrhouma <>

---
+++ src/main/java/com/Tn/WeDev/WeDevApplication.java
@@ -0,0 +1,15 @@
+package com.Tn.WeDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeDevApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/Tn/WeDev/WeDevApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Tn.WeDev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeDevApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
